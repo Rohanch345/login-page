111a04d668782bf8c7abd41f57fde22fa5f73f1c
rohanch345 <rohanch345@gmail.com>
2025-02-08 08:00:31 -0500

    Initialize commit

---
+++ app/src/main/java/com/random/login_2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.random.login_2;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+
+    Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(username.getText().toString().equals("user") && password.getText().toString().equals("1234"))
+                {
+                    Toast.makeText(MainActivity.this,"Login Succesful!",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Login Failed" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
